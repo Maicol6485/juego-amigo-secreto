c06fc6b5b46ad434371853f7b6c83109fbe905a2
Maicol6485 <michael.solis.alarcon@gmail.com>
2025-08-26 22:48:42 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+// Utilizar const cuando la variable no cambia ej; nombres
+const input = document.getElementById('amigo');
+const lista = document.getElementById('listaAmigos');
+const resultado = document.getElementById('resultado');
